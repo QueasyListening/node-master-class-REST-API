4c11aff28f23bde892ed64e7367fbf279ce7652c
joshuacoyne <josh.j.coyne@gmail.com>
2018-08-21 16:46:00 -0400

    Finished Returning JSON

---
+++ index.js
@@ -0,0 +1,103 @@
+/*
+* Primary file for the API
+*
+*/
+
+//Dependencies
+const http = require('http');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
+// The server should respond to all requests with a string
+
+// Start the server and have it listen on port 3000
+const server = http.createServer(function(req, res){
+    
+    // Get url and parse it
+    let parsedUrl = url.parse(req.url, true);
+
+    // Get the path
+    let path = parsedUrl.pathname;
+    let trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+    // Get the query sting as an objcet
+    let queryStringObject = parsedUrl.query;
+    // Get the HTTP method
+    let method = req.method.toLowerCase();
+
+    // Get the headers as an object
+    let headers = req.headers;
+
+    // Get the payload, if any
+    let decoder = new StringDecoder('utf-8');
+    let buffer = '';
+
+    req.on('data', function(data){
+        buffer += decoder.write(data);
+    });
+
+    req.on('end', function(){
+        buffer += decoder.end();
+
+        // choose the handler this req should go to 
+        // Use notFound if not found
+        let chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound; 
+
+        // Construct the data object to send to the handler
+        let data = {
+            'trimmedPath' : trimmedPath,
+            'queryStringObject' : queryStringObject,
+            'method' : method,
+            'headers' : headers,
+            'payload' : buffer
+        };
+
+        // Route the request to the handler specified in the router
+        chosenHandler(data, function(statusCode,payload){
+            // Use the status code calledback by the handler or default to 200
+            statusCode = typeof(statusCode) === 'number' ? statusCode : 200;
+
+            // Use the payload calledback by the handler or default to and empty object
+            payload = typeof(payload) === 'object' ? payload : {};
+
+            // convert the payload to a string
+            let payloadString = JSON.stringify(payload);
+            res.setHeader('Content-Type','application/json');
+            res.writeHead(statusCode);
+            
+            // Send the response
+            res.end(payloadString);
+
+            // Log the request path
+            console.log('Returning this response: ', payloadString);
+            
+        });
+
+    
+    });
+
+    
+});
+
+server.listen(3000, function(){
+    console.log("The server is listening on port 3000");
+});
+
+// Deifine handlers
+let handlers = {};
+
+// Sample handler
+handlers.sample = function(data, callback){
+    // Callback an http status code and a payload object
+    callback(406, { 'name': 'sample handler' });
+};
+
+// Not found handler
+handlers.notFound = function(data, callback){
+    callback(404);
+};
+
+// Define a request router
+let router = {
+    'sample' : handlers.sample,
+    'notFound' : handlers.notFound
+}
